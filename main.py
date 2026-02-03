3da5aa9064bf2bced06beba17f2d589de574b6b4
Kaho Legradi <legradi.kaho+github@gmail.com>
2026-02-03 10:27:33 +0000

    changes

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
-from flask import Flask, render_template
-
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-@app.route('/')
+UPLOAD_FOLDER = "static/uploads"
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return "." in filename and \
+           filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template('home.html')
+    image_url = None
+    colors = None
+
+    if request.method == "POST":
+        file = request.files.get("image")
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            file.save(filepath)
+
+            image_url = url_for("static", filename=f"uploads/{filename}")
+
+            # TODO: extract colours here
+            # colors = extract_colors(filepath)
+
+    return render_template(
+        "home.html",
+        image_url=image_url,
+        colors=colors
+    )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
