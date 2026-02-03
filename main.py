c5d4a40106123b70e1c8ed0408a89a13533f1ca5
Kaho Legradi <legradi.kaho+github@gmail.com>
2026-02-03 16:24:09 +0000

    changes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
+from PIL import Image
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = "static/uploads"
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+base_width = 600
 
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
@@ -21,16 +23,27 @@ def home():
 
     if request.method == "POST":
         file = request.files.get("image")
+        image = Image.open(file)
+        width_percent = (base_width / float(image.size[0]))
+        height_size = int((float(image.size[1]) * float(width_percent)))
+        image = image.resize((base_width, height_size), Image.Resampling.LANCZOS)
 
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-            file.save(filepath)
+            image.save(filepath)
 
             image_url = url_for("static", filename=f"uploads/{filename}")
 
-            # TODO: extract colours here
-            # colors = extract_colors(filepath)
+            image.save(filepath)
+
+            image_url = url_for(
+                "static",
+                filename=f"uploads/{filename}"
+            )
+
+            colors = image.getdata()
+            print(colors)
 
     return render_template(
         "home.html",
